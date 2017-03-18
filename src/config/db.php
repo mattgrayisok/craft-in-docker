342503d2e588361dde02c94335963f4b21bc5862
Matt Gray <matt.gray@retrofuzz.com>
2017-03-18 00:10:51 +0000

    Initial Commit

---
+++ src/config/db.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+
+return array(
+
+    'server' => 'mysql',
+    'database' => 'project',
+    'user' => 'project',
+    'password' => 'project',
+    'tablePrefix' => 'craft'
+
+);
